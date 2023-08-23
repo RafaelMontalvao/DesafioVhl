6b109529ecf209629ea0e3eb32d9173824d1527f
Rafael Brito <rushbig@hotmail.com>
2023-08-22 21:43:54 -0300

    iniciando projeto

---
+++ src/main/java/desafio/vhl/com/desafiovhl/DesafioVhlApplication.java
@@ -0,0 +1,13 @@
+package desafio.vhl.com.desafiovhl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioVhlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioVhlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/desafio/vhl/com/desafiovhl/DesafioVhlApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.vhl.com.desafiovhl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVhlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
