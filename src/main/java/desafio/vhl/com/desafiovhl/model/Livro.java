0c5b5278964ecdc5a23988d1040412c99e4ee409
Rafael Brito <rushbig@hotmail.com>
2023-08-22 22:14:17 -0300

    Classe Livro Criada

---
+++ src/main/java/desafio/vhl/com/desafiovhl/model/Livro.java
@@ -0,0 +1,22 @@
+package desafio.vhl.com.desafiovhl.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "LIVROS")
+public class Livro {
+
+    @Id
+    private String isbn;
+
+    private String titulo;
+
+    private String autor;
+
+    private boolean disponivel;
+}
