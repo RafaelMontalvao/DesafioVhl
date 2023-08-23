07e3a0562bad8e2982241d355fc51eed13b95c29
Rafael Brito <rushbig@hotmail.com>
2023-08-22 22:09:14 -0300

    Classe usuario criada

---
+++ src/main/java/desafio/vhl/com/desafiovhl/model/Usuario.java
@@ -0,0 +1,23 @@
+package desafio.vhl.com.desafiovhl.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "USUARIOS")
+@Data
+public class Usuario {
+
+    @Id
+    private Long cep;
+
+    private String nome;
+
+    private LocalDate dataNascimento;
+
+    private int qtdemprestimos;
+}
